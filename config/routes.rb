1c6cc002e0d99abfeaf20dd11ddbfcf1ff702216
Tanesaka <seedtanesa@outlook.jp>
2020-12-14 13:55:25 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :customers
   root to: 'public/homes#top'
 
+# 12/14種坂　下記、namespace実装で不要になるかもしれません
+# また、新規登録画面のみの記載です。ログイン、ログアウトはなぜか指定せずとも動きました。
   get 'customers/sign_up' => 'customers/registrations#new'
 end
