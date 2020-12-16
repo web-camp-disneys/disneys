90f7a8b4466c40d4db2bac03c055dba57aa575e4
EC2 Default User <ec2-user@ip-172-31-81-100.ec2.internal>
2020-12-16 07:13:40 +0000

    Solve update error

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -13,12 +13,17 @@ class Admins::CustomersController < ApplicationController
   
   def update
     @customer = Customer.find(params[:id])
-    if @customer.update
-      redirect_to admin_customer_path(@customer.id),
+    if @customer.update(customer_params)
+      redirect_to admins_customer_path(@customer.id),
       flash:{notice:'You have updated customer info successfully.'}
     else
       render 'edit'
     end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 
 end
