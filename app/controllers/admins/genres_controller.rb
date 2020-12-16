c2669ad80ec8ceeef8296791fba9ed40a4091b95
Tanesaka <seedtanesa@outlook.jp>
2020-12-16 10:13:52 +0000

    Update

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -12,9 +12,13 @@ class Admins::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admins_genres_path
   end
 
   def destroy
