8041c4123656c508aa10b67861f2114a31c6f9f5
Tanesaka <seedtanesa@outlook.jp>
2020-12-16 07:47:43 +0000

    GenresFirst

---
+++ app/controllers/admins/genres_controller.rb
@@ -0,0 +1,31 @@
+class Admins::GenresController < ApplicationController
+
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admins_genres_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @genre = Genre.find(params[:id])
+    @genre.destroy
+    redirect_to admins_genres_path
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name, :is_active)
+  end
+end
