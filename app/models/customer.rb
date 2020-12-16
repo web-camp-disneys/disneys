bb683c609ccfb9751374c75a10f8f3209cd8afcf
EC2 Default User <ec2-user@ip-172-31-81-100.ec2.internal>
2020-12-16 07:32:30 +0000

    AdminCustomerPages done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,12 @@
 class Customer < ApplicationRecord
+  
+  enum is_deleted: {Available: false, Invalid: true}
+    #有効会員はfalse、退会済み会員はtrue
+
+    def deleted_for_authentication?
+        super && (self.is_deleted === "Invalid")
+    end
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
