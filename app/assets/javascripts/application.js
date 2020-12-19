7a0ab7a97892b2aa3bad01cb4590d7b46df656d2
okushogg <okusho721@gmail.com>
2020-12-19 08:16:44 +0000

    Improve headerdesign

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
