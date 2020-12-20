b47cee932fa17281bd2b227c311bfbd84c1f204f
okushogg <okusho721@gmail.com>
2020-12-20 05:41:18 +0000

    Improve public/top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,4 +1,9 @@
 class Public::HomesController < ApplicationController
   def top
+    @items = Item.where(is_active: false)
+    @genres = Genre.where(is_active: true)
+    if params[:genre_id].present?
+      @genre = Genre.find(params[:genre_id])
+    end
   end
 end
