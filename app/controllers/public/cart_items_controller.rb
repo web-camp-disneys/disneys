0ea18ccfee6d4ec5bcedc9a70e3e340993084be0
okushogg <okusho721@gmail.com>
2020-12-22 12:07:23 +0000

    Add confirm

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -21,7 +21,7 @@ class Public::CartItemsController < ApplicationController
   end
 
  def index
-   @cart_items = CartItem.all
+   @cart_items = current_customer.cart_items
  end
 
  def update
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,6 +9,33 @@ class Public::OrdersController < ApplicationController
     @new_name = @order.name
   end
 
+  
+
+  def confirm
+    @order = Order.new
+    @order.payment_method = params[:order][:payment_method].to_i 
+    @cart_items = current_customer.cart_items
+    
+    # ご自身の住所を選択した場合
+    if params[:order][:shipping_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = "#{current_customer.last_name} #{current_customer.first_name}"
+    # 登録済み住所から選択した場合
+    elsif params[:order][:shipping_address] == "1"
+      @chosen_address = Address.find_by(params[:order][:address_id])
+      @order.postal_code = @chosen_address.postal_code
+      @order.address = @chosen_address.address
+      @order.name = @chosen_address.name
+    # 新しいお届け先を選択した場合
+    else
+      @order.postal_code = params[:order][:new_postal_code]
+      @order.address = params[:order][:new_address]
+      @order.name = params[:order][:new_name]
+    end
+    
+  end
+  
   def create
     @order = Order.new(order_params)
     # 登録済み住所選択プルダンに使用
@@ -17,33 +44,12 @@ class Public::OrdersController < ApplicationController
     @new_address = @order.address
     @new_postal_code = @order.postal_code
     @new_name = @order.name
-   
-    
-    if params[:order][:shipping_address] == "0"
-      @order.postal_code = current_customer.postal_code
-      @order.address = current_customer.address
-      @order.name = "#{current_customer.last_name} #{current_customer.first_name}"
-    elsif params[:order][:shipping_address] == "1"
-      @order.postal_code = @chosen_address.postal_code
-      @order.address = @chosen_address.address
-      @order.name = @chosen_address.name
-    else 
-      @new_postal_code = @order.postal_code
-      @new_address = @order.address
-      @new_name = @order.name
-    end
-    
-    @order.save
-    redirect_to complete_public_orders_path
   end
-
-  def show
-  end
-
+  
   def index
   end
 
-  def confirm
+  def show
   end
 
   def complete
