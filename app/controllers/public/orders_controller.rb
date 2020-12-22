a4a482b09138dec7263d0ddf24679e44a39788b4
okushogg <okusho721@gmail.com>
2020-12-22 06:18:52 +0000

    Clear address part

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,24 +1,36 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
+    # 登録済み住所選択プルダンに使用
     @addresses = Address.where(customer: current_customer)
+    # 新規入力住所に使用
+    @new_address = @order.address
+    @new_postal_code = @order.postal_code
+    @new_name = @order.name
   end
 
   def create
     @order = Order.new(order_params)
-
+    # 登録済み住所選択プルダンに使用
+    @chosen_address = Address.find_by(params[:order][:address_id])
+    # 新規入力住所に使用
+    @new_address = @order.address
+    @new_postal_code = @order.postal_code
+    @new_name = @order.name
+   
+    
     if params[:order][:shipping_address] == "0"
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = "#{current_customer.last_name} #{current_customer.first_name}"
     elsif params[:order][:shipping_address] == "1"
-      @order.postal_code = current_customer.address_id.postal_code
-      @order.address = current_customer.address_id.address
-      @order.name = current_customer.address_id.name
+      @order.postal_code = @chosen_address.postal_code
+      @order.address = @chosen_address.address
+      @order.name = @chosen_address.name
     else 
-      @address = Address.new(address_params)
-      @address.customer_id = current_customer.id
-      @address.save
+      @new_postal_code = @order.postal_code
+      @new_address = @order.address
+      @new_name = @order.name
     end
     
     @order.save
