4943f358e71f88f8198af120a5de4e772b88a2d0
Tanesaka <seedtanesa@outlook.jp>
2020-12-23 12:57:53 +0000

    fix

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -20,9 +20,12 @@ class Admins::GenresController < ApplicationController
   end
 
   def update
-    genre = Genre.find(params[:id])
-    genre.update(genre_params)
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
     redirect_to admins_genres_path
+    else
+      render "edit"
+    end
   end
 
   def destroy
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ApplicationRecord
-    validates :name,:is_active, presence: true
+    validates :name, presence: true
     validates :is_active, inclusion: { in: [true, false] }
 
     has_many :items
