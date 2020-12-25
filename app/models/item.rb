08394240e467f31247e389f4ed3500d5d1b17298
Tanesaka <seedtanesa@outlook.jp>
2020-12-25 05:25:46 +0000

    Update

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
     attachment :image
-    validates :name, :genre_id, :introduction, :image, :price, :is_active, presence: true
+    validates :name, :genre_id, :introduction, :price, presence: true
 
     belongs_to :genre
     has_many :cart_items
