b68c61984094b34e64857d90edfc27acf6f3ace8
Tanesaka <seedtanesa@outlook.jp>
2020-12-26 12:11:59 +0000

    First

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,6 @@ ActiveRecord::Schema.define(version: 2020_12_22_180557) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "users"
-    t.boolean "admin"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
